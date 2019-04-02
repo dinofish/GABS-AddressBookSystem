46cbd0ebe3ca49f93632557eb98c088ae0477fc6
dinofish <dinofish@users.noreply.github.com>
2019-04-02 21:06:04 +0800

    Update lang_controller.php
    修复数据库json数据返回错误

--- src/controller/lang_controller.php
+++ src/controller/lang_controller.php
@@ -22,11 +22,11 @@ $stmt->bind_param('ss', $pageName, $lang);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$responce = '';
+$responce = array();
 $i = 0;
 
 while ($row = $result->fetch_assoc()) {
-    $responce[$i] = [
+    $responce[] = [
     'XuHao' => $row['XU_HAO'],
     'WenZi' => $row['WEN_ZI'],
   ];
